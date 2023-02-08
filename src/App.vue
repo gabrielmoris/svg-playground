8a27bfd5032013db49eaafbfecd5862e4f12152a
gabriel <gabrielcmoris@gmail.com>
2023-02-08 11:07:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-red-900 text-bold text-3xl">
+      {{ endpoint }}
+    </h1>
+    <!-- <image :src="svg" /> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+// import axios from "axios";
+import * as svg from "./assets/response.svg";
+const endpoint = ref("Hello World");
+</script>
