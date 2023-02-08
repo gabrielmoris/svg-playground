7370127040c03e0e7819456c5d7ca897412cafd9
gabriel <gabrielcmoris@gmail.com>
2023-02-08 13:36:20 +0100

    render iframe

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
+  <div class="flex justify-center items-center flex-col">
     <h1 class="text-red-900 text-bold text-3xl">
       {{ endpoint }}
     </h1>
-    <!-- <image :src="svg" /> -->
+
+    <iframe
+      class="w-full h-screen flex justify-center items-center flex-col"
+      :src="'../public/response.svg'"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 // import axios from "axios";
-import * as svg from "./assets/response.svg";
+import svg from "../public/response.svg";
+console.log(svg);
 const endpoint = ref("Hello World");
 </script>
