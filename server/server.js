576380dcaf999ca877390d2b49d33a3b03f23e99
gabriel <gabrielcmoris@gmail.com>
2023-02-10 07:49:38 +0100

    add auth route, salplan route

--- server/server.js
+++ server/server.js
@@ -3,34 +3,70 @@ import dotenv from "dotenv";
 import axios from "axios";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { stringify } from "qs";
 
 const port = 3002;
 const app = express();
 dotenv.config();
+function hasCookie(req, res, next) {
+  console.log("headers auth", req.headers.authorization);
+  if (req.headers.authorization) {
+    next();
+  } else {
+    let configToken = {
+      method: "post",
+      url: process.env.VITE_ENDPOINT_TOKEN,
+      headers: {
+        Authorization: process.env.VITE_AUTHORIZATION,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: data,
+    };
+
+    axios(configToken)
+      .then(function (response) {
+        res
+          .cookie("refresh_token", response.data.refresh_token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: response.data.refresh_expires_in * 600000,
+          })
+          .json({ Authorization: response.data.access_token });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    next();
+  }
+}
+
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(cookieParser());
+app.use(hasCookie);
 
-import { stringify } from "qs";
 var data = stringify({
   username: process.env.VITE_WBS_USERNAME,
   password: process.env.VITE_WBS_PASSWORD,
   grant_type: "password",
 });
-var configToken = {
-  method: "post",
-  url: process.env.VITE_ENDPOINT_TOKEN,
-  headers: {
-    Authorization: process.env.VITE_AUTHORIZATION,
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-  data: data,
-};
 
 app.get("/", async (req, res) => {
-  axios(configToken)
+  res.status(200);
+});
+
+app.get("/saalplan", async (req, res) => {
+  var config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://test-eras.stage-entertainment.de/shows/28410470/floorplan?hidePrices=0&withoutJS=0&withPromotions=false",
+    headers: {
+      accept: "application/svg+xml",
+      Authorization: req.headers.authorization,
+    },
+  };
+  axios(config)
     .then(function (response) {
-      res.status(200).send(response.data);
-      // console.log(JSON.stringify(response.data));
+      res.status(200).send(JSON.stringify(response.data));
     })
     .catch(function (error) {
       console.log(error);
--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,17 @@
       class="mt-20 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
       @click="access"
     >
-      Access
+      {{ buttonName }}
     </button>
+
+    <button
+      class="mt-20 bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded"
+      @click="getSaalplan"
+    >
+      Saalplan
+    </button>
+
+    <div class="flex items-center justify-center pt-20" id="container"></div>
 
     <!-- <iframe
       class="w-full h-screen flex justify-center items-center flex-col"
@@ -20,6 +29,7 @@
     /> -->
 
     <!-- <object
+      v-if="svg"
       class="w-full h-screen flex justify-center items-center flex-col"
       :data="'/response.svg'"
     >
@@ -33,19 +43,40 @@
 import { ref } from "vue";
 import axios from "axios";
 axios.defaults.withCredentials = true;
-const endpoint = ref("http://localhost:3002");
+const endpoint = ref("http://localhost:3002/");
+const buttonName = ref("Login");
+const svg = ref();
 
 const access = async () => {
   const { data } = await axios.get(endpoint.value);
-  axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
-  console.log(data);
+  axios.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${data.Authorization}`;
+  console.log(JSON.parse(data));
+};
+
+const getSaalplan = async () => {
+  endpoint.value = "http://localhost:3002/saalplan";
+  const { data } = await axios.get(endpoint.value);
+  axios.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${data.Authorization}`;
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(data, "application/xml");
+  // svg.value = doc;
+  const container = document.getElementById("container");
+  if (container != null)
+    container.appendChild(
+      container.ownerDocument.importNode(doc.documentElement, true)
+    );
+  console.timeLog(data);
 };
 </script>
 
 <style>
 #TX86 {
-  fill: blue;
+  fill: pink;
   font: bolder;
-  font-size: xx-large;
+  filter: drop-shadow(3px 3px 2px #cb44dd);
 }
 </style>
